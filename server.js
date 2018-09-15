f91641ffd2675ec9770593276a2b9120996061e3
Justin Beckwith <beckwith@google.com>
2018-09-14 18:52:54 -0700

    Only publish PushEvent type events

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const topic = pubsub.topic(topicName);
 const publisher = topic.publisher();
 
 async function poll(tailId) {
+
+  // Fetch all public GitHub events.  We can only grab 100 at a time.
   const url = `https://api.github.com/events?per_page=${perPage}`;
   const res = await axios(url, {
     headers: {
@@ -15,26 +17,37 @@ async function poll(tailId) {
     }
   });
 
+  // Make sure were not running out of API calls.
   const remaining = res.headers['x-ratelimit-remaining'];
   if (remaining <= 0) {
     console.log(`We are out of requests to the GitHub API!`)
   }
 
+  // Assume that we're going to get a lot of overlapping data.
+  // Trip the list to only include new IDs we haven't seen before.
   const ids = res.data.map(x => x.id);
   const firstId = ids[0];
   const iom = ids.indexOf(tailId);
+  const events = res.data.slice(0, iom);
 
+  // If the trimmed list is the same as the page size, there are more
+  // than 100 events every 0.75 seconds 👻
   if (iom === perPage) {
     console.log(`Too many results returned, we are losing data.`);
   }
 
-  const events = res.data.slice(0, iom);
+  // Iterate through each public event, and publish PushEvent type
+  // through the pubsub topic.  Something else will do stuff with em.
   events.forEach(async x => {
-    const messageId = await publisher.publish(Buffer.from(JSON.stringify(x)));
-    console.log(`${x.id}:${messageId}`);
+    if (x.type === 'PushEvent') {
+      const messageId = await publisher.publish(Buffer.from(JSON.stringify(x)));
+      console.log(`${x.id}:${messageId}`);
+    }
   });
   console.log('---');
 
+  // GitHub provides 5000 API calls per hour.  That works out to about one
+  // every 720 milliseconds.  Poll on 750 to be as aggressive as possible.
   setTimeout(() => {
     poll(firstId);
   }, 750);
